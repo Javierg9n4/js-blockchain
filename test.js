7cdef46381fb3302386e761cb29d26c6f8587944
Javierg9n4 <javierg9n4@gmail.com>
2022-11-19 16:51:48 +0100

    initial commit, blockchain class created and first test executed

---
+++ test.js
@@ -0,0 +1,157 @@
+const Blockchain = require("./blockchain");
+
+let bitcoin = new Blockchain();
+
+bitcoin.createNewTransaction(
+  "100",
+  "0xBcd4042DE499D14e55001CcbB24a551F3b954096",
+  "0xa0Ee7A142d267C1f36714E4a8F75612F20a79720"
+);
+bitcoin.createNewTransaction(
+  "100",
+  "0xBcd4042DE499D14e55001CcbB24a551F3b954096",
+  "0xa0Ee7A142d267C1f36714E4a8F75612F20a79720"
+);
+bitcoin.createNewTransaction(
+  "100",
+  "0xBcd4042DE499D14e55001CcbB24a551F3b954096",
+  "0xa0Ee7A142d267C1f36714E4a8F75612F20a79720"
+);
+bitcoin.createNewTransaction(
+  "100",
+  "0xBcd4042DE499D14e55001CcbB24a551F3b954096",
+  "0xa0Ee7A142d267C1f36714E4a8F75612F20a79720"
+);
+bitcoin.createNewTransaction(
+  "100",
+  "0xBcd4042DE499D14e55001CcbB24a551F3b954096",
+  "0xa0Ee7A142d267C1f36714E4a8F75612F20a79720"
+);
+bitcoin.createNewTransaction(
+  "100",
+  "0xBcd4042DE499D14e55001CcbB24a551F3b954096",
+  "0xa0Ee7A142d267C1f36714E4a8F75612F20a79720"
+);
+bitcoin.createNewTransaction(
+  "100",
+  "0xBcd4042DE499D14e55001CcbB24a551F3b954096",
+  "0xa0Ee7A142d267C1f36714E4a8F75612F20a79720"
+);
+bitcoin.createNewTransaction(
+  "100",
+  "0xBcd4042DE499D14e55001CcbB24a551F3b954096",
+  "0xa0Ee7A142d267C1f36714E4a8F75612F20a79720"
+);
+bitcoin.createNewTransaction(
+  "100",
+  "0xBcd4042DE499D14e55001CcbB24a551F3b954096",
+  "0xa0Ee7A142d267C1f36714E4a8F75612F20a79720"
+);
+bitcoin.createNewTransaction(
+  "100",
+  "0xBcd4042DE499D14e55001CcbB24a551F3b954096",
+  "0xa0Ee7A142d267C1f36714E4a8F75612F20a79720"
+);
+bitcoin.createNewTransaction(
+  "100",
+  "0xBcd4042DE499D14e55001CcbB24a551F3b954096",
+  "0xa0Ee7A142d267C1f36714E4a8F75612F20a79720"
+);
+bitcoin.createNewTransaction(
+  "100",
+  "0xBcd4042DE499D14e55001CcbB24a551F3b954096",
+  "0xa0Ee7A142d267C1f36714E4a8F75612F20a79720"
+);
+bitcoin.createNewTransaction(
+  "100",
+  "0xBcd4042DE499D14e55001CcbB24a551F3b954096",
+  "0xa0Ee7A142d267C1f36714E4a8F75612F20a79720"
+);
+bitcoin.createNewTransaction(
+  "100",
+  "0xBcd4042DE499D14e55001CcbB24a551F3b954096",
+  "0xa0Ee7A142d267C1f36714E4a8F75612F20a79720"
+);
+
+bitcoin.createNewBLock();
+
+bitcoin.createNewTransaction(
+  "100",
+  "0xBcd4042DE499D14e55001CcbB24a551F3b954096",
+  "0xa0Ee7A142d267C1f36714E4a8F75612F20a79720"
+);
+bitcoin.createNewTransaction(
+  "100",
+  "0xBcd4042DE499D14e55001CcbB24a551F3b954096",
+  "0xa0Ee7A142d267C1f36714E4a8F75612F20a79720"
+);
+bitcoin.createNewTransaction(
+  "100",
+  "0xBcd4042DE499D14e55001CcbB24a551F3b954096",
+  "0xa0Ee7A142d267C1f36714E4a8F75612F20a79720"
+);
+bitcoin.createNewTransaction(
+  "100",
+  "0xBcd4042DE499D14e55001CcbB24a551F3b954096",
+  "0xa0Ee7A142d267C1f36714E4a8F75612F20a79720"
+);
+bitcoin.createNewTransaction(
+  "100",
+  "0xBcd4042DE499D14e55001CcbB24a551F3b954096",
+  "0xa0Ee7A142d267C1f36714E4a8F75612F20a79720"
+);
+bitcoin.createNewTransaction(
+  "100",
+  "0xBcd4042DE499D14e55001CcbB24a551F3b954096",
+  "0xa0Ee7A142d267C1f36714E4a8F75612F20a79720"
+);
+bitcoin.createNewTransaction(
+  "100",
+  "0xBcd4042DE499D14e55001CcbB24a551F3b954096",
+  "0xa0Ee7A142d267C1f36714E4a8F75612F20a79720"
+);
+bitcoin.createNewTransaction(
+  "100",
+  "0xBcd4042DE499D14e55001CcbB24a551F3b954096",
+  "0xa0Ee7A142d267C1f36714E4a8F75612F20a79720"
+);
+bitcoin.createNewTransaction(
+  "100",
+  "0xBcd4042DE499D14e55001CcbB24a551F3b954096",
+  "0xa0Ee7A142d267C1f36714E4a8F75612F20a79720"
+);
+bitcoin.createNewTransaction(
+  "100",
+  "0xBcd4042DE499D14e55001CcbB24a551F3b954096",
+  "0xa0Ee7A142d267C1f36714E4a8F75612F20a79720"
+);
+bitcoin.createNewTransaction(
+  "100",
+  "0xBcd4042DE499D14e55001CcbB24a551F3b954096",
+  "0xa0Ee7A142d267C1f36714E4a8F75612F20a79720"
+);
+bitcoin.createNewTransaction(
+  "100",
+  "0xBcd4042DE499D14e55001CcbB24a551F3b954096",
+  "0xa0Ee7A142d267C1f36714E4a8F75612F20a79720"
+);
+bitcoin.createNewTransaction(
+  "100",
+  "0xBcd4042DE499D14e55001CcbB24a551F3b954096",
+  "0xa0Ee7A142d267C1f36714E4a8F75612F20a79720"
+);
+bitcoin.createNewTransaction(
+  "100",
+  "0xBcd4042DE499D14e55001CcbB24a551F3b954096",
+  "0xa0Ee7A142d267C1f36714E4a8F75612F20a79720"
+);
+
+bitcoin.createNewBLock();
+
+
+console.log(bitcoin);
+
+console.log("\n");
+console.log("Second Block transactions", bitcoin.chain[1].transactions);
+console.log("\n");
+console.log("Third Block transactions", bitcoin.chain[2].transactions);
